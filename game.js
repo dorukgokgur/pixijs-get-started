5c549f36416d027a4e5124205c379e1a037fa395
Doruk Gökgür <dorukgokgur@gmail.com>
2023-01-05 12:14:59 +0300

    PixiJS Get Started

---
+++ game.js
@@ -0,0 +1,548 @@
+let character = {
+  frames: {
+    "tile000.png": {
+      frame: { x: 0, y: 0, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile001.png": {
+      frame: { x: 56, y: 0, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile002.png": {
+      frame: { x: 0, y: 56, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile003.png": {
+      frame: { x: 56, y: 56, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile004.png": {
+      frame: { x: 0, y: 112, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile005.png": {
+      frame: { x: 56, y: 112, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile008.png": {
+      frame: { x: 0, y: 168, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile009.png": {
+      frame: { x: 56, y: 168, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile010.png": {
+      frame: { x: 0, y: 224, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile011.png": {
+      frame: { x: 56, y: 224, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile012.png": {
+      frame: { x: 0, y: 280, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile013.png": {
+      frame: { x: 56, y: 280, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile014.png": {
+      frame: { x: 0, y: 336, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile015.png": {
+      frame: { x: 56, y: 336, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile016.png": {
+      frame: { x: 0, y: 392, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile017.png": {
+      frame: { x: 56, y: 392, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile018.png": {
+      frame: { x: 0, y: 448, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile019.png": {
+      frame: { x: 56, y: 448, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile020.png": {
+      frame: { x: 0, y: 504, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile021.png": {
+      frame: { x: 56, y: 504, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile022.png": {
+      frame: { x: 0, y: 560, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile023.png": {
+      frame: { x: 56, y: 560, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile024.png": {
+      frame: { x: 0, y: 616, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile025.png": {
+      frame: { x: 56, y: 616, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile026.png": {
+      frame: { x: 0, y: 672, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile027.png": {
+      frame: { x: 56, y: 672, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile028.png": {
+      frame: { x: 0, y: 728, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile029.png": {
+      frame: { x: 56, y: 728, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile030.png": {
+      frame: { x: 0, y: 784, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile031.png": {
+      frame: { x: 56, y: 784, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile032.png": {
+      frame: { x: 0, y: 840, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile033.png": {
+      frame: { x: 56, y: 840, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile034.png": {
+      frame: { x: 0, y: 896, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile035.png": {
+      frame: { x: 56, y: 896, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile036.png": {
+      frame: { x: 0, y: 952, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile037.png": {
+      frame: { x: 56, y: 952, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile038.png": {
+      frame: { x: 0, y: 1008, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile039.png": {
+      frame: { x: 56, y: 1008, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile040.png": {
+      frame: { x: 0, y: 1064, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile041.png": {
+      frame: { x: 56, y: 1064, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile042.png": {
+      frame: { x: 0, y: 1120, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile043.png": {
+      frame: { x: 56, y: 1120, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile048.png": {
+      frame: { x: 0, y: 1176, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile049.png": {
+      frame: { x: 56, y: 1176, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile050.png": {
+      frame: { x: 0, y: 1232, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile051.png": {
+      frame: { x: 56, y: 1232, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile052.png": {
+      frame: { x: 0, y: 1288, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile053.png": {
+      frame: { x: 56, y: 1288, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile054.png": {
+      frame: { x: 0, y: 1344, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile055.png": {
+      frame: { x: 56, y: 1344, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile056.png": {
+      frame: { x: 0, y: 1400, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile057.png": {
+      frame: { x: 56, y: 1400, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile058.png": {
+      frame: { x: 0, y: 1456, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile059.png": {
+      frame: { x: 56, y: 1456, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile064.png": {
+      frame: { x: 0, y: 1512, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile065.png": {
+      frame: { x: 56, y: 1512, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile066.png": {
+      frame: { x: 0, y: 1568, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile067.png": {
+      frame: { x: 56, y: 1568, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile068.png": {
+      frame: { x: 0, y: 1624, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile069.png": {
+      frame: { x: 56, y: 1624, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile070.png": {
+      frame: { x: 0, y: 1680, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile071.png": {
+      frame: { x: 56, y: 1680, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile072.png": {
+      frame: { x: 0, y: 1736, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile073.png": {
+      frame: { x: 56, y: 1736, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile074.png": {
+      frame: { x: 0, y: 1792, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile080.png": {
+      frame: { x: 56, y: 1792, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile081.png": {
+      frame: { x: 0, y: 1848, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+    "tile082.png": {
+      frame: { x: 56, y: 1848, w: 56, h: 56 },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: { x: 0, y: 0, w: 56, h: 56 },
+      sourceSize: { w: 56, h: 56 },
+    },
+  },
+  animations: {
+    idle: ["tile000.png", "tile001.png", "tile002.png", "tile003.png", "tile004.png", "tile005.png"],
+    attack: ["tile008.png", "tile009.png", "tile010.png", "tile011.png", "tile012.png", "tile013.png", "tile014.png", "tile015.png"],
+    run: ["tile016.png", "tile017.png", "tile018.png", "tile019.png", "tile020.png", "tile021.png", "tile022.png", "tile023.png"],
+  },
+  meta: {
+    app: "https://www.codeandweb.com/texturepacker",
+    version: "1.0",
+    image: "./spriteSheets/character/character.png",
+    format: "RGBA8888",
+    size: { w: 112, h: 1904 },
+    scale: "1",
+    smartupdate: "$TexturePacker:SmartUpdate:30655878fb6edf00507856dbe19dc1ea:c527453e73a4f117e0250537c3bd3ea2:42e475aa4a03af306bdc7acb5ff91d19$",
+  },
+};
+
+let createApp = async () => {
+  //Pixi App Açılışı
+  let app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight, backgroundColor: "black" });
+
+  //Body'e Pixi App'ini ekleme
+  document.body.appendChild(app.view);
+
+  //Karakter objesinin oluşturulması
+  const spritesheet = new PIXI.Spritesheet(PIXI.BaseTexture.from(character.meta.image), character);
+
+  //Karakter objesinin parse edilmesi
+  await spritesheet.parse();
+
+  //Container oluşturma
+  const runContainer = new PIXI.Container();
+  const idleContainer = new PIXI.Container();
+  const attackContainer = new PIXI.Container();
+
+  //Animasyonlar
+  const animIdle = new PIXI.AnimatedSprite(spritesheet.animations.idle);
+  const animAttack = new PIXI.AnimatedSprite(spritesheet.animations.attack);
+  const animRun = new PIXI.AnimatedSprite(spritesheet.animations.run);
+
+  //Animasyon hızları
+  animAttack.animationSpeed = 0.1666;
+  animIdle.animationSpeed = 0.1666;
+  animRun.animationSpeed = 0.1666;
+
+  //Animasyonların oynatılması
+  animAttack.play();
+  animIdle.play();
+  animRun.play();
+
+  //Animasyonların containerlara eklenmesi
+  runContainer.addChild(animRun);
+  idleContainer.addChild(animIdle);
+  attackContainer.addChild(animAttack);
+
+  //Containerların pozisyonları
+  runContainer.x = 50;
+  attackContainer.y = 50;
+
+  //Containerların alpha değerleri
+  attackContainer.alpha = 0.5;
+
+  //Containerların ekrana eklenmesi
+  app.stage.addChild(idleContainer);
+  app.stage.addChild(attackContainer);
+  app.stage.addChild(runContainer);
+};
+
+//Pixi App'ini oluşturmak için DOMContentLoaded eventi
+document.addEventListener("DOMContentLoaded", createApp);
